3ecd18d43f9a7b420e3f04b319bca08448d4ede7
gustavojardim <gustavo.spadotto@gmail.com>
2020-11-24 16:38:06 -0300

    Update settings.py

--- front_end/historico_digital/historico_digital/settings.py
+++ front_end/historico_digital/historico_digital/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')1)c%jdm60bo=7snsnx(4*3pzu8@ibjh3z!hk^4r#mp6h(u@h8'
+SECRET_KEY = os.getenv('HD_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
